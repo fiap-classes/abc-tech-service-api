acd60ea7e55d8931088eea11ee16a1482fc718be
Luiz Zenha <lzenha@gmail.com>
2022-03-31 21:10:57 -0300

    OrderDto + OrderApplcation + Flyway

--- src/test/java/br/com/fiap/abctechservice/service/OrderServiceTest.java
+++ src/test/java/br/com/fiap/abctechservice/service/OrderServiceTest.java
@@ -2,6 +2,7 @@ package br.com.fiap.abctechservice.service;
 
 import br.com.fiap.abctechservice.model.Assistance;
 import br.com.fiap.abctechservice.model.Order;
+import br.com.fiap.abctechservice.repository.AssistanceRepository;
 import br.com.fiap.abctechservice.repository.OrderRepository;
 import br.com.fiap.abctechservice.service.impl.OrderServiceImpl;
 import org.aspectj.weaver.ast.Or;
@@ -15,22 +16,25 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 public class OrderServiceTest {
 
     @Mock
     private OrderRepository orderRepository;
+    @Mock
+    private AssistanceRepository assistanceRepository;
     private OrderService orderService;
 
     @BeforeEach
     public void init() {
         MockitoAnnotations.openMocks(this);
 
-        orderService = new OrderServiceImpl(orderRepository);
+        orderService = new OrderServiceImpl(orderRepository, assistanceRepository);
+        when(assistanceRepository.findById(any())).thenReturn(Optional.of(new Assistance(1L, "Teste", "Teste Description")));
     }
 
     @Test
@@ -42,8 +46,8 @@ public class OrderServiceTest {
     public void create_order_success() throws Exception {
         Order newOrder = new Order();
         newOrder.setOperatorId(1234L);
-        newOrder.setServices(generate_mock_assistance(1));
-        orderService.saveOrder(newOrder);
+
+        orderService.saveOrder(newOrder, generate_mock_assistance(1));
 
 
         verify(orderRepository, times(1)).save(newOrder);
@@ -53,10 +57,9 @@ public class OrderServiceTest {
     public void create_order_error_minimum() throws Exception {
         Order newOrder = new Order();
         newOrder.setOperatorId(1234L);
-        newOrder.setServices(List.of());
 
 
-        Assertions.assertThrows(ArrayIndexOutOfBoundsException.class, () -> orderService.saveOrder(newOrder));
+        Assertions.assertThrows(ArrayIndexOutOfBoundsException.class, () -> orderService.saveOrder(newOrder, List.of()));
         verify(orderRepository, times(0)).save(newOrder);
     }
 
@@ -64,16 +67,15 @@ public class OrderServiceTest {
     public void create_order_error_maximum() throws Exception {
         Order newOrder = new Order();
         newOrder.setOperatorId(1234L);
-        newOrder.setServices(generate_mock_assistance(20));
 
-        Assertions.assertThrows(ArrayIndexOutOfBoundsException.class, () -> orderService.saveOrder(newOrder));
+        Assertions.assertThrows(ArrayIndexOutOfBoundsException.class, () -> orderService.saveOrder(newOrder, generate_mock_assistance(20)));
         verify(orderRepository, times(0)).save(newOrder);
     }
 
-    private List<Assistance> generate_mock_assistance(int number) {
-        ArrayList<Assistance> arrayList = new ArrayList<>();
+    private List<Long> generate_mock_assistance(int number) {
+        ArrayList<Long> arrayList = new ArrayList<>();
         for (int x = 0; x < number; x++){
-            arrayList.add(new Assistance(Integer.toUnsignedLong(x), "Mock service name", "Mock service description"));
+            arrayList.add(Integer.toUnsignedLong(x));
         }
         return  arrayList;
     }
